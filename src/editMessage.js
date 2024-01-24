80bb6e5b9beebe2bf1c244c94b11837d737ef0f8
Nessie <63102193+Nessie-Dev@users.noreply.github.com>
2024-01-24 15:46:29 +0600

    Update editMessage.js

--- src/editMessage.js
+++ src/editMessage.js
@@ -1,3 +1,33 @@
+var requestCounter = 1; // Initialize the counter
 
+// Function to get the current epoch timestamp
+function getCurrentEpochTimestamp() {
+    return Math.floor(new Date().getTime() / 1000); // Convert milliseconds to seconds
+};
 
-module.exports = 
+module.exports = function (defaultFuncs, api, ctx) {
+  const mqttClient = ctx.mqttClient;
+   var currentRequestID = requestCounter++;
+
+    var payloadToSend = {
+        "app_id": "2220391788200892",
+        "payload": {
+            "tasks": [
+                {
+                    "label": "742",
+                    "payload": {
+                        "message_id": message_id,
+                        "text": text
+                    },
+                    "queue_name": "edit_message",
+                    "task_id": currentRequestID,
+                    "failure_count": null
+                }
+            ],
+            "epoch_id": getCurrentEpochTimestamp(),
+            "version_id": "7849679408382587"
+        },
+        "request_id": currentRequestID,
+        "type": 3 // Assuming this field remains constant for this type of request
+    };
+}
