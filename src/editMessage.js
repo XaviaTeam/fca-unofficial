40291759bd784cb8c460113099ff51b9471603ea
Nessie <63102193+Nessie-Dev@users.noreply.github.com>
2024-01-24 23:39:26 +0600

    Update editMessage.js

--- src/editMessage.js
+++ src/editMessage.js
@@ -9,32 +9,59 @@ function getCurrentEpochTimestamp() {
 
 module.exports = function (defaultFuncs, api, ctx) {
   const mqttClient = ctx.mqttClient;
-   var currentRequestID = requestCounter++;
-   return function editMessage(messageID, text, callback) {
-    var payloadToSend = {
-        "app_id": "2220391788200892",
-        "payload": {
-            "tasks": [
-                {
-                    "label": "742",
-                    "payload": {
-                        "message_id": messageID,
-                        "text": text
-                    },
-                    "queue_name": "edit_message",
-                    "task_id": currentRequestID,
-                    "failure_count": null
-                }
-            ],
-            "epoch_id": getCurrentEpochTimestamp(),
-            "version_id": "7849679408382587"
-        },
-        "request_id": currentRequestID,
-        "type": 3 // Assuming this field remains constant for this type of request
-    };
-       mqttClient.publish('ls_req', JSON.stringify(payloadToSend), {
-        qos: 1,
-        retain: false,
+  var currentRequestID = requestCounter++;
+
+  return function editMessage(messageID, text, callback) {
+    var resolveFunc = function () {};
+    var rejectFunc = function () {};
+    var returnPromise = new Promise(function (resolve, reject) {
+      resolveFunc = resolve;
+      rejectFunc = reject;
     });
-   }
-}
+
+    if (!callback) {
+      callback = function (err) {
+        if (err) {
+          return rejectFunc(err);
+        }
+        resolveFunc();
+      };
+    }
+
+    var payloadToSend = {
+      "app_id": "2220391788200892",
+      "payload": {
+        "tasks": [
+          {
+            "label": "742",
+            "payload": {
+              "message_id": messageID,
+              "text": text
+            },
+            "queue_name": "edit_message",
+            "task_id": currentRequestID,
+            "failure_count": null
+          }
+        ],
+        "epoch_id": getCurrentEpochTimestamp(),
+        "version_id": "7849679408382587"
+      },
+      "request_id": currentRequestID,
+      "type": 3
+    };
+
+    mqttClient.publish('ls_req', JSON.stringify(payloadToSend), {
+      qos: 1,
+      retain: false,
+    });
+
+    returnPromise
+      .then(function () {
+        callback(); // Resolve the promise when the operation is successful
+      })
+      .catch(function (err) {
+        callback(err); // Reject the promise if there's an error
+      });
+  };
+};
+
